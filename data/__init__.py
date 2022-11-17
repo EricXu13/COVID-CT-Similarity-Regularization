c7463f8035cc3eda3a34b732b54dbcf17041dbb2
a411919924 <a411919924@icloud.com>
2022-11-17 15:59:01 +0000

    deleted redundant files

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,2 @@
 # from .build import build_loader, build_transform
-from .build_new import build_loader, build_dataset, build_external_dataset, build_transform, build_mixup_fn
+from .build import build_loader, build_dataset, build_external_dataset, build_transform, build_mixup_fn
--- data/build.py
+++ data/build.py
@@ -8,9 +8,9 @@ from torch.utils.data import DataLoader, DistributedSampler
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.data import Mixup
 from timm.data import create_transform
-from timm.data.transforms import str_to_pil_interp
+from timm.data.transforms import _pil_interp
 
-from .covid_dataset import COVIDxDataset
+from .covid_dataset import COVIDxDataset, ExternalDataset
 from .samplers import SubsetRandomSampler
 from .utils import TwoCropTransform
 
@@ -47,16 +47,7 @@ def build_loader(config):
         test_set, sampler=test_sampler, batch_size=config.DATA.BATCH_SIZE, shuffle=False, \
         num_workers=config.DATA.NUM_WORKERS, pin_memory=config.DATA.PIN_MEMORY, drop_last=False)
     
-    # setup mixup / cutmix
-    mixup_fn = None
-    mixup_active = (config.AUG.MIXUP > 0 or config.AUG.CUTMIX > 0. 
-                    or config.AUG.CUTMIX_MINMAX is not None) and (config.AUG.TYPE == 'auto')
-    if mixup_active:
-        mixup_fn = Mixup(
-            mixup_alpha=config.AUG.MIXUP, cutmix_alpha=config.AUG.CUTMIX, \
-            cutmix_minmax=config.AUG.CUTMIX_MINMAX, prob=config.AUG.MIXUP_PROB, \
-            switch_prob=config.AUG.MIXUP_SWITCH_PROB, mode=config.AUG.MIXUP_MODE, \
-            label_smoothing=config.MODEL.LABEL_SMOOTHING, num_classes=config.MODEL.NUM_CLASSES)
+    mixup_fn = build_mixup_fn(config)
 
     return train_loader, val_loader, test_loader, mixup_fn
     
@@ -67,70 +58,99 @@ def build_dataset(split, config):
     dataset = COVIDxDataset(config.DATA.DIR, split_file, transform)
     return dataset
 
+def build_external_dataset(config):
+    transform = build_transform(train=False, config=config)
+    dataset = ExternalDataset(config.DATA.DIR, transform)
+    return dataset
+
 
 def build_transform(train, config):
-    resize_im = config.DATA.IMG_SIZE > 32
     if train:
-        return build_train_transform(config, resize_im)
+        return build_train_transform_by_level(config)
            
     t = []
-    if resize_im:
-        if config.TEST.CROP:
-            size = int((256 / 224) * config.DATA.IMG_SIZE)
-            t.append(transforms.Resize(size, interpolation=str_to_pil_interp(config.DATA.INTERPOLATION)))
-            t.append(transforms.CenterCrop(config.DATA.IMG_SIZE))
-        else:
-            t.append(transforms.Resize((config.DATA.IMG_SIZE, config.DATA.IMG_SIZE),
-                                  interpolation=str_to_pil_interp(config.DATA.INTERPOLATION)))
+    if config.TEST.CROP:
+        size = int((256 / 224) * config.DATA.IMG_SIZE)
+        t.append(transforms.Resize(size, interpolation=_pil_interp(config.DATA.INTERPOLATION)))
+        t.append(transforms.CenterCrop(config.DATA.IMG_SIZE))
+    else:
+        t.append(transforms.Resize((config.DATA.IMG_SIZE, config.DATA.IMG_SIZE),
+                              interpolation=_pil_interp(config.DATA.INTERPOLATION)))
 
     t.append(transforms.ToTensor())
     t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
     return transforms.Compose(t)
 
 
-def build_train_transform(config, resize_im):
-    if config.AUG.TYPE == 'auto':
+def build_train_transform_by_level(config):
+#     Augmentation Level:
+#     0: No augmentation
+#     1: + Random Resized Crop, scale=(0.08, 1.0), ratio=(3./4., 4./3.)
+#     2: + Horizontal Flipping, p=0.5
+#     3: + Randn Augmentation, "rand-m9-mstd0.5-inc1"
+#     4: + Random Erasing, p=0.25, mode=pixel, recount=1
+#     5: + Cutmix, alpha=1.0
+#     6. + Mixup, cutmix switch to mixup with prob=0.5, mixup alpha = 0.8
+    level = config.AUG.LEVEL
+    if level == 0:
         transform = create_transform(
-            input_size = config.DATA.IMG_SIZE,
-            is_training=True,
-            color_jitter=config.AUG.COLOR_JITTER if config.AUG.COLOR_JITTER > 0 else None,
-            auto_augment=config.AUG.AUTO_AUGMENT if config.AUG.AUTO_AUGMENT != 'none' else None,
-            re_prob=config.AUG.REPROB,
-            re_mode=config.AUG.REMODE,
-            re_count=config.AUG.RECOUNT,
-            interpolation=config.DATA.INTERPOLATION,)
-    elif config.AUG.TYPE == 'none':
-        transform = create_transform(
-            input_size = config.DATA.IMG_SIZE,
+            input_size=config.DATA.IMG_SIZE,
             is_training=True,
             no_aug=True,
             auto_augment=None,
             interpolation=config.DATA.INTERPOLATION,)
-    elif config.AUG.TYPE == 'simple':
+    elif level == 1:
         transform = create_transform(
-            input_size = config.DATA.IMG_SIZE,
+            input_size=config.DATA.IMG_SIZE,
             is_training=True,
-            hflip=0.5,
-            color_jitter=config.AUG.COLOR_JITTER if config.AUG.COLOR_JITTER > 0 else None,
-            auto_augment=None,
+            hflip=0.,
+            color_jitter=None,
             interpolation=config.DATA.INTERPOLATION,)
-    elif config.AUG.TYPE == 'simple-no-color':
+    elif level == 2:
         transform = create_transform(
-            input_size = config.DATA.IMG_SIZE,
+            input_size=config.DATA.IMG_SIZE,
             is_training=True,
             hflip=0.5,
             color_jitter=None,
-            auto_augment=None,
+            interpolation=config.DATA.INTERPOLATION,)
+    elif level == 3:
+        transform = create_transform(
+            input_size=config.DATA.IMG_SIZE,
+            is_training=True,
+            auto_augment=config.AUG.AUTO_AUGMENT,
             interpolation=config.DATA.INTERPOLATION,)
     else:
-        raise NotImplementedError("Unknown arg for data augmentation")
-    
-    if not resize_im:
-        transform.transforms[0] = transforms.RandomCrop(config.DATA.IMG_SIZE, padding=4)
+        # level >= 4
+        transform = create_transform(
+            input_size=config.DATA.IMG_SIZE,
+            is_training=True,
+            auto_augment=config.AUG.AUTO_AUGMENT,
+            re_prob=config.AUG.REPROB,
+            re_mode=config.AUG.REMODE,
+            re_count=config.AUG.RECOUNT,
+            interpolation=config.DATA.INTERPOLATION,)
+        
+    if level > 6 or level < 0:
+        raise NotImplementedError("Unknown data augmentation level, valid in range [0, 6]")
+        
     if config.TRAIN.TWOCROP:
         transform = TwoCropTransform(transform)
         
     return transform
+
+
+def build_mixup_fn(config):
+    # setup mixup / cutmix
+    mixup_fn = None
+    if config.AUG.LEVEL > 4: 
+        cutmix_enable = config.AUG.LEVEL == 6
+        mixup_fn = Mixup(
+            mixup_alpha=config.AUG.MIXUP, cutmix_alpha=config.AUG.CUTMIX, \
+            cutmix_minmax=config.AUG.CUTMIX_MINMAX, prob=config.AUG.MIXUP_PROB, \
+            switch_prob=config.AUG.MIXUP_SWITCH_PROB if cutmix_enable else 0., \
+            mode=config.AUG.MIXUP_MODE, \
+            label_smoothing=config.MODEL.LABEL_SMOOTHING, num_classes=config.MODEL.NUM_CLASSES)
+    return mixup_fn
         
     
 
